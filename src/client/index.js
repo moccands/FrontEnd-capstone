90deec5cde369e99184cc3cb1a9694763796f6c0
moccands <moccandsayeghsara@gmail.com>
2020-06-05 20:15:59 +0200

    rm css + add if null

--- src/client/index.js
+++ src/client/index.js
@@ -3,13 +3,10 @@ import { getCountDown } from './js/nameChecker'
 import { getHistDate } from './js/nameChecker'
 import { getGeo, postData, getWeath, udpateUI } from './js/formHandler'
 
-import './styles/resets.scss'
 import './styles/header.scss'
 import './styles/form.scss'
-import './styles/footer.scss'
 import './styles/base.scss'
 import './styles/imagecity.scss'
-
 
 export {
     getCountDown,
@@ -18,25 +15,26 @@ export {
 
  
 const handleSubmit = async (e) => {
-    e.preventDefault();
+  e.preventDefault();
 
-    const baseURL = 'http://api.geonames.org/postalCodeSearchJSON?placename='
-    const apiUser = '&username=moccands';
-    let formText = document.getElementById('name').value
+  const baseURL = 'http://api.geonames.org/postalCodeSearchJSON?placename='
+  const apiUser = '&username=moccands';
+  let formText = document.getElementById('name').value
 
-    console.log("::: Form Submitted :::")
+  console.log("::: Form Submitted :::")
 
+  if (formText) {
     getGeo(baseURL,formText, apiUser).
-      then(function(data){
+      then(function(data) {
         getWeath(data).
-          then(function(dataWeath){
+          then(function(dataWeath) {
             postData('http://localhost:8081/getImage', {data : formText }).then(function(res) {
               console.log(res)
               udpateUI(dataWeath,res.text)
              });
           });
       });
-  };
+  }
+};
   
-
 document.getElementById("submitform").addEventListener("click", handleSubmit);
